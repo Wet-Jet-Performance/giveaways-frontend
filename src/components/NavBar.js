5a20c1790ae8d3461ff268bb579b1d8fef6c7253
kendall holmes <121322044+kendalliscoding@users.noreply.github.com>
2024-04-12 13:54:35 -0700

    add navbar

---
+++ src/components/NavBar.js
@@ -0,0 +1,26 @@
+import { NavLink } from 'react-router-dom';
+import './NavBar.css';
+import { useState } from 'react';
+
+const NavBar = () => {
+  const [showLinks, setShowLinks] = useState(false);
+  const linksClass = showLinks ? 'show-links' : 'hide-links';
+
+  return (
+    <nav className={linksClass}>
+      <div id='hamburger-icon' onClick={() => setShowLinks(!showLinks)}>
+        <div id='hamburger-bar1'></div>
+        <div id='hamburger-bar2'></div>
+        <div id='hamburger-bar3'></div>
+      </div>
+      <div className='nav-links'>
+        <NavLink to='/'>HOME</NavLink>
+        <NavLink to='/giveaways'>GIVEAWAYS</NavLink>
+        <NavLink to='/admin'>ADMIN</NavLink>
+      </div>
+      <p id='site-title'> Wet Jet Performance </p>
+    </nav>
+  );
+};
+
+export default NavBar;
