77f4ec20623d04577e6f609eb36ec805ee1071e3
kendall holmes <121322044+kendalliscoding@users.noreply.github.com>
2024-05-06 20:51:03 -0700

    clean up and syntax

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -1,9 +1,8 @@
-import { NavLink } from 'react-router-dom';
 import { useAuth0 } from '@auth0/auth0-react';
 import "./Footer.css";
 
 const Footer = ({admin_page_active}) => {
-  const {loginWithRedirect, isAuthenticated} = useAuth0();
+  const {loginWithRedirect} = useAuth0();
   
   const admin_button_class = admin_page_active ? 'active' : '';
 
@@ -17,9 +16,9 @@ const Footer = ({admin_page_active}) => {
 
   return (
     <footer>
-      <a className={admin_button_class} onClick={loginRedirect}>
+      <button className={admin_button_class} onClick={loginRedirect}>
           ADMIN
-      </a>
+      </button>
       <div className="copyright">&copy; Wet Jet Performance</div>
     </footer>
   );
