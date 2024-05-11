84e6137d65916a41182cabef12727387c49883be
kendall holmes <121322044+kendalliscoding@users.noreply.github.com>
2024-05-10 18:06:24 -0700

    style home page with logos

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -1,19 +1,21 @@
-import { NavLink } from "react-router-dom";
 import Footer from "../components/Footer";
 import './Home.css'
+import NavBar from "../components/NavBar";
+import WJPShield from "../assets/wjp-shield.png";
 
 const Home = () => {
 
   return (
     <div className='home-body'>
-      <div className='black-background' />
+      <NavBar />
       <div id='landing-titles'>
+        <img className='wjp-shield' src={WJPShield} alt='wet jet performance shield logo'/>
         <h1 className='site-title'>Wet Jet Performance</h1>
         <h2 className='site-title'>Giveaways</h2>
       </div>
-      <div id='landing-links'>
-        <NavLink id='current-giveaways-link' to="/giveaways">Current Giveaways</NavLink>
-        <a href='https://wetjetperformance.com/' id='more-wjp-link'>More Wet Jet Performance</a>
+      <div id='landing-more-wjp'>
+        <p> Looking for more Wet Jet Performance?</p>
+        <p> Check out <a href='https://wetjetperformance.com/' id='more-wjp-link' target='_blank' rel='noreferrer'> wetjetperformance.com </a></p>
       </div>
       <Footer />
     </div>
