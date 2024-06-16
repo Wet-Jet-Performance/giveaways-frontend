27042bfb56c7c9ec854710775fb3c3d6e020b4ce
kendall holmes <121322044+kendalliscoding@users.noreply.github.com>
2024-06-16 12:53:54 -0700

    update navbar logo to shield

--- src/components/NavBar.js
+++ src/components/NavBar.js
@@ -2,6 +2,7 @@ import { NavLink } from 'react-router-dom';
 import './NavBar.css';
 import { useState } from 'react';
 import WJPLogo from '../assets/wjp.png'
+import WJPShield from '../assets/wjp-shield.png'
 
 const NavBar = () => {
   const [showLinks, setShowLinks] = useState(false);
@@ -19,7 +20,7 @@ const NavBar = () => {
         <NavLink to='/giveaways'>GIVEAWAYS</NavLink>
       </div>
       <div className='nav-title'>
-        <img className='wjp-logo' src={WJPLogo} alt='wet jet performance logo' />
+        <img className='wjp-logo' src={WJPShield} alt='wet jet performance logo' />
         <p id='giveaway-title'> GIVEAWAYS </p>
       </div>
     </nav>
